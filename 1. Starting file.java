76344bc223311387ba3afb8429ffbaba1e943ebd
chunte <chunte@gmail.com>
2014-06-25 11:36:24 +0800

    Created Starting file

--- 1. Starting file.java
+++ 1. Starting file.java
@@ -1,3 +1,5 @@
+package tutorial.mod;
+
 /* Imports  */
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -5,5 +7,15 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 /* Needed for forge mods */
-@Mod(modid = "MonkeyMods", name = "MonkeyKing Mods", version = "v1")
+@Mod(modid = "TutorialMod", name = "Tutorial Mod", version = "v1")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
+
+public class TutorialMod {
+	
+	@EventHandler
+	public void load(FMLInitializationEvent event)
+	{
+		
+	}
+
+}
